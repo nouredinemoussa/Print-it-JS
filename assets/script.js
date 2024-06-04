a9d476cb937f667b734505bc72aa04658452cd3b
nouredinemoussa <nouredine.moussa1@gmail.com>
2024-06-04 15:52:53 +0200

    Défilement des textes et des bullet points

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,53 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
-let bannerImg = document.querySelector(".banner-img");
+const bannerImg = document.querySelector(".banner-img");
 const leftClick = document.querySelector(".arrow_left");
-leftClick.addEventListener ('click',function(){console.log('Bouton gauche cliqué')});
-console.log (leftClick);
-
 const rightClick = document.querySelector(".arrow_right");
-rightClick.addEventListener ('click', function(){
-	console.log('Bouton droit cliqué')
-});
+const bulletPoints = document.querySelector(".dot");
+const tagLine = document.querySelector("#banner p");
+
+const imgDot = () => {
+    const dots = document.querySelectorAll('.dot');
+    dots.forEach((dot, index) => {
+        dot.classList.toggle('dot_selected', index === bannerIndex);
+    });
+};
+
+let bannerIndex = 0;
+
+bannerImg.setAttribute ("src", slides[bannerIndex].image);
+tagLine.innerHTML = slides[bannerIndex].tagLine;
+
+const nextImage = () => {
+	bannerIndex = (bannerIndex + 1) % slides.length;
+	bannerImg.setAttribute ("src", slides[bannerIndex].image);
+	tagLine.innerHTML = slides[bannerIndex].tagLine;
+	imgDot();
+}
+const previousImage = () => {
+	bannerIndex = (bannerIndex - 1) % slides.length;
+	bannerImg.setAttribute ("src", slides[bannerIndex].image);
+	tagLine.innerHTML = slides[bannerIndex].tagLine;
+	imgDot();
+}
+leftClick.addEventListener ('click', previousImage);
+console.log (leftClick);
+rightClick.addEventListener ('click',nextImage);
 console.log (rightClick);
+
